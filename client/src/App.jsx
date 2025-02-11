e58b2a6c84589d673cf943cfffee1da7a5289abd
Shilizen <shilizenkv@gmail.com>
2025-02-12 01:31:41 +0530

    last test worked trying again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,83 @@
 
+// import React, { useEffect } from "react";
+// import { Navigate, Route, Routes } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { authCheck } from "./Redux/AuthSlice";
+// import {
+//   connectSocket,
+//   disconnectSocket,
+// } from "./SocketService/SocketIoService";
+// import Navbar from "./Components/Navbar";
+// import HomePage from "./Pages/HomePage";
+// import Register from "./Pages/Register";
+// import Login from "./Pages/Login";
+// import Profile from "./Pages/Profile";
+// import Settings from "./Pages/Settings";
+
+// import { Toaster } from "react-hot-toast";
+// import Home from "./Components/Home";
+// import Search from "./Components/Search";
+// import AudioCalls from "./Components/AudioCalls";
+// import VideoCalls from "./Components/VideoCalls";
+
+// const App = () => {
+//   const { user, isAuthenticated} = useSelector(
+//     (state) => state.auth
+//   );
+//   const dispatch = useDispatch();
+//   // console.log(user, isAuthenticated, isLoading, onlineUsers);
+//   useEffect(() => {
+//     dispatch(authCheck());
+
+//     let socket;
+//     if (user && isAuthenticated) {
+//       socket = connectSocket(user.id, dispatch);
+//     }
+
+//     return () => {
+//       if (socket) {
+//         disconnectSocket();
+//       }
+//     };
+//   }, [dispatch, user?.id]);
+
+
+
+//   return (
+//     <div className="w-full h-screen ">
+//       {/* <Navbar /> */}
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}
+//         >
+//           <Route index path="/" element={<Home />} />
+//           <Route path="search" element={<Search />} />
+//           <Route path="call" element={<AudioCalls />} />
+//           <Route path="video-call" element={<VideoCalls />} />
+//           <Route path="settings" element={<Settings />} />
+//           <Route
+//             path="profile"
+//             element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+//           />
+//         </Route>
+//         <Route
+//           path="/register"
+//           element={!isAuthenticated ? <Register /> : <Navigate to="/" />}
+//         />
+//         <Route
+//           path="/login"
+//           element={!isAuthenticated ? <Login /> : <Navigate to="/" />}
+//         />
+//       </Routes>
+//       <Toaster />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React, { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -39,7 +118,7 @@ const App = () => {
         disconnectSocket();
       }
     };
-  }, [user?.id]);
+  }, [dispatch, user?.id]);
 
 
 
--- server/lib/GenerateToken.js
+++ server/lib/GenerateToken.js
@@ -8,9 +8,9 @@ exports.GenerateToken = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true, // This cookie cannot be accessed by client-side JavaScript
     secure: process.env.NODE_ENV === "production" ? true : false, // This cookie can only be sent over https
-    sameSite: "strict", // This cookie is not sent along with any cross-site requests
+    // sameSite: "strict", // This cookie is not sent along with any cross-site requests
     maxAge: 1 * 60 * 60 * 1000, // 1 hour
-    
+    sameSite: "None",
   });
 
   return token; // Make sure to return the token
